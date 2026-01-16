64e8092122277a4f4c1cd9bff67d9fe6be029577
Lohan Boeglin <lohan.boeglin@etu.univ-nantes.fr>
2026-01-16 20:26:45 +0100

    fix: suppression import inutilisé

--- reservations/src/test/kotlin/iut/nantes/project/reservations/controller/ReservationControllerTest.kt
+++ reservations/src/test/kotlin/iut/nantes/project/reservations/controller/ReservationControllerTest.kt
@@ -1,7 +1,6 @@
 package iut.nantes.project.reservations.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -10,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
